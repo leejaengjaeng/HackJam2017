c1933b3865e7dfdff2f3480e4d5b7163fefe8723
ljh6274@naver.com <ljh6274@naver.com>
2017-06-24 05:37:08 +0900

    a

--- src/main/java/com/hackjam/controller/BotPushController.java
+++ src/main/java/com/hackjam/controller/BotPushController.java
@@ -1,20 +1,17 @@
 package com.hackjam.controller;
 
-import com.hackjam.bo.BotMessageUtil;
-import com.hackjam.bo.BotOrderProcessBo;
-import com.hackjam.constant.BotOrderStep;
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.JoinEvent;
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.PostbackEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.util.LinkedMultiValueMap;
@@ -24,7 +21,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.http.HttpServletRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
 
 /**
  * Created by Jaeyoung Lee on 2017. 6. 18..
@@ -32,57 +31,62 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class BotPushController {
 
-    private static final Logger logger = LoggerFactory.getLogger(BotPushController.class);
+	private static final Logger logger = LoggerFactory.getLogger(BotPushController.class);
 
-    @RequestMapping("/botPush")
-    public void pushDoneMessage(@RequestParam String userId){
-        String t = "QTCRhvV69pDaO89Va0KmKFQGQbldX42mbzbXu1f+NJIGQo+qVZcFe/SuUn7s529/GeQTJYgp3N88TFLoFpPmXRTvKKkhhf/2jQVXlYchb6biE5qamkwDYQIz9364yO1UBXSj2XltadtlR3in6e9DDAdB04t89/1O/w1cDnyilFU=";
-        logger.info("@@@UserID : "+userId);
+	@RequestMapping("/botPush")
+	public String pushDoneMessage(@RequestParam String userId) throws Exception {
+		logger.info("@@@UserID : " + userId);
 
-        MultiValueMap<String,String> header  = new LinkedMultiValueMap();
-        header.add("Content-Type","application/json");
-        header.add("Authorization","Bearer "+t);
+		MultiValueMap<String, Object> body = new LinkedMultiValueMap();
+		body.add("to", userId);
+		Message pushMessages[] = {new TextMessage("음료가 완성되었습니다.\n받으러 와주세요~")};
+		body.add("messages", pushMessages);
 
+		String t = "QTCRhvV69pDaO89Va0KmKFQGQbldX42mbzbXu1f+NJIGQo+qVZcFe/SuUn7s529/GeQTJYgp3N88TFLoFpPmXRTvKKkhhf/2jQVXlYchb6biE5qamkwDYQIz9364yO1UBXSj2XltadtlR3in6e9DDAdB04t89/1O/w1cDnyilFU=";
+		HttpPost httpPost = new HttpPost("https://api.line.me/v2/bot/message/push");
+		httpPost.setHeader("Content-Type", "application/json");
+		httpPost.setHeader("Authorization", "Bearer " + t);
 
-        MultiValueMap<String,Object> body  = new LinkedMultiValueMap();
-        body.add("to",userId);
-        Message pushMessages[] = {new TextMessage("음료가 완성되었습니다.\n받으러 와주세요~")};
-        body.add("messages",pushMessages);
+		ObjectMapper objectMapper = new ObjectMapper();
+		String bodyString = objectMapper.writeValueAsString(body);
+		StringEntity stringEntity = new StringEntity(bodyString);
+		httpPost.setEntity(stringEntity);
 
-        RestTemplate callPush = new RestTemplate();
-        callPush.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+			CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+			return EntityUtils.toString(httpResponse.getEntity());
+		} catch (IOException ioe) {
+			logger.error("{}", ioe);
+			return ioe.getMessage();
+		}
+	}
 
-        HttpEntity<MultiValueMap<String,Object>> request = new HttpEntity<>(body,header);
+	@RequestMapping("/botPush2")
+	public void pushDoneMessage2(@RequestParam String userId) {
+		String t = "QTCRhvV69pDaO89Va0KmKFQGQbldX42mbzbXu1f+NJIGQo+qVZcFe/SuUn7s529/GeQTJYgp3N88TFLoFpPmXRTvKKkhhf/2jQVXlYchb6biE5qamkwDYQIz9364yO1UBXSj2XltadtlR3in6e9DDAdB04t89/1O/w1cDnyilFU=";
+		logger.info("@@@UserID : " + userId);
 
-        callPush.postForObject("https://api.line.me/v2/bot/message/push",request,String.class);
-    }
-    @RequestMapping("/botPush2")
-    public void pushDoneMessage2(@RequestParam String userId){
-        String t = "QTCRhvV69pDaO89Va0KmKFQGQbldX42mbzbXu1f+NJIGQo+qVZcFe/SuUn7s529/GeQTJYgp3N88TFLoFpPmXRTvKKkhhf/2jQVXlYchb6biE5qamkwDYQIz9364yO1UBXSj2XltadtlR3in6e9DDAdB04t89/1O/w1cDnyilFU=";
-        logger.info("@@@UserID : "+userId);
+		MultiValueMap<String, String> header = new LinkedMultiValueMap();
+		header.add("Content-Type", "application/json");
+		header.add("Authorization", "Bearer " + t);
 
-        MultiValueMap<String,String> header  = new LinkedMultiValueMap();
-        header.add("Content-Type","application/json");
-        header.add("Authorization","Bearer "+t);
+		MultiValueMap<String, Object> body = new LinkedMultiValueMap();
+		body.add("to", userId);
+		Message pushMessages[] = {new TextMessage("음료가 완성되었습니다.\n받으러 와주세요~")};
+		body.add("messages", pushMessages);
 
+		RestTemplate callPush = new RestTemplate();
+		callPush.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 
-        MultiValueMap<String,Object> body  = new LinkedMultiValueMap();
-        body.add("to",userId);
-        Message pushMessages[] = {new TextMessage("음료가 완성되었습니다.\n받으러 와주세요~")};
-        body.add("messages",pushMessages);
+		HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(body, header);
 
-        RestTemplate callPush = new RestTemplate();
-        callPush.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		callPush.postForObject("http://localhost:8080/kkkk", request, String.class);
+	}
 
-        HttpEntity<MultiValueMap<String,Object>> request = new HttpEntity<>(body,header);
+	@RequestMapping("/kkkk")
+	public void dasdasd(HttpServletRequest req) {
 
-        callPush.postForObject("http://localhost:8080/kkkk",request,String.class);
-    }
+		String a = "";
 
-    @RequestMapping("/kkkk")
-    public void dasdasd(HttpServletRequest req){
-
-        String a="";
-
-    }
+	}
 }
